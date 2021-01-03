57589dc164d8f9302081fb6353464566ca392314
jaovito <junior.vitorio.dias@gmail.com>
2021-01-03 11:24:29 -0300

    add emailservice

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,20 @@
+import User from '@modules/users/infra/typeorm/entities/User';
+import AppError from '@shared/errors/AppError';
+import { injectable, inject } from 'tsyringe';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UserRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {}
+}
+
+export default SendForgotPasswordEmailService;
