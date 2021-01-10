0bde0b5f6e333c52b5d6098e507215ada2018e1c
jaovito <junior.vitorio.dias@gmail.com>
2021-01-10 12:53:14 -0300

    list provider day

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,6 +1,5 @@
 import { injectable, inject } from 'tsyringe';
 
-import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import User from '@modules/users/infra/typeorm/entities/User';
@@ -10,21 +9,19 @@ interface IRequest {
 }
 
 @injectable()
-class ShowProfileService {
+class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
   ) {}
 
-  public async execute({ user_id }: IRequest): Promise<User> {
-    const user = await this.usersRepository.findById(user_id);
-
-    if (!user) {
-      throw new AppError('User not found.');
-    }
+  public async execute({ user_id }: IRequest): Promise<User[]> {
+    const user = await this.usersRepository.findAllProviders({
+      except_id: user_id,
+    });
 
     return user;
   }
 }
 
-export default ShowProfileService;
+export default ListProvidersService;
